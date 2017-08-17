83b45dc4eb13da46c0fe4d7fa799dd4923fbfef8
Xun Xu <xuxu@pc-131-62.customer.hammersmith.generation.lan>
2017-08-17 22:44:35 +0100

    config

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,7 +1,7 @@
 ﻿var express = require('express');
 var router = express.Router();
 var request = require('request');
-var config = require('config.json');
+var config = require('config.js');
 
 router.get('/', function (req, res) {
     // log user out
