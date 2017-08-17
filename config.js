c5f6fa3ecf24d5b8ffb1fe190dee0c5893f3f54f
Xun Xu <xuxu@pc-131-62.customer.hammersmith.generation.lan>
2017-08-17 23:33:43 +0100

    config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   "connectionString": process.env.PROD_MONGODB,
-  "apiUrl": "https://imperialfair.herokuapp.com/",
+  "apiUrl": "https://imperialfair.herokuapp.com/api",
   "secret": "2017 Msc Individual Project"
 };
